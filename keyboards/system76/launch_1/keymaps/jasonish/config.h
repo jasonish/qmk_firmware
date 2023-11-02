a22b017977f5ab11ac812a9c8807ee222b40ef5e
Jason Ish <jason@codemonkey.net>
2023-11-02 10:59:37 -0600

    gmail-fix

--- keyboards/system76/launch_1/keymaps/jasonish/config.h
+++ keyboards/system76/launch_1/keymaps/jasonish/config.h
@@ -1,6 +1,10 @@
 #define TAPPING_TERM 240
 #define QUICK_TAP_TERM 100
 
+// Add more delay here, otherwise Gmail doesn't always register home row mod
+// taps. I don't see this with any other apps though.
+#define TAP_CODE_DELAY 30
+
 // Override the default to a solid color.
 #undef RGB_MATRIX_DEFAULT_MODE
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
